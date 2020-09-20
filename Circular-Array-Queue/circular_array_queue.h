ac4b8bfde4f66818f22e2a4eeb5c4afe326682fe
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-09-20 11:00:53 -0300

    circular queue passing all tests

--- Circular-Array-Queue/circular_array_queue.h
+++ Circular-Array-Queue/circular_array_queue.h
@@ -1,12 +1,12 @@
 #ifndef STRUCTURES_CIRCULAR_ARRAY_QUEUE_H_
-#define STRUCTURES_CIRCULAR_ARRAY_QUEUE_H_
+  #define STRUCTURES_CIRCULAR_ARRAY_QUEUE_H_
 
-#include <cstdint>
-#include <stdexcept>
+  #include <cstdint>
+  #include <stdexcept>
 
 namespace structures {
 template <typename T>
-//! Classe ArrayQueue
+//! Classe CircularArrayQueue
 /* Classe fila em vetor com tipo genérico, sem movimentação de dados.
  *
  * Classe fila em vetor, com tipo génerico, sem movimentação de dados,
@@ -95,3 +95,85 @@ class CircularArrayQueue {
 }  // namespace structures
 
 #endif
+
+template <typename T>
+structures::CircularArrayQueue<T>::CircularArrayQueue(void) {
+  max_size_ = DEFAULT_SIZE;
+  begin_ = 0;
+  end_ = -1;
+  size_ = 0;
+  contents = new T[max_size_];
+}
+
+template <typename T>
+structures::CircularArrayQueue<T>::CircularArrayQueue(std::size_t max_size) {
+  max_size_ = max_size;
+  begin_ = 0;
+  end_ = -1;
+  size_ = 0;
+  contents = new T[max_size_];
+}
+
+template <typename T>
+structures::CircularArrayQueue<T>::~CircularArrayQueue(void) {
+  delete[] contents;
+}
+
+template <typename T>
+void structures::CircularArrayQueue<T>::enqueue(const T& data) {
+  if (full()) {
+    throw(std::out_of_range("Cannot enqueue on full queue"));
+  } else {
+    end_ = (end_ + 1) % max_size_;
+    contents[end_] = data;
+    size_++;
+  }
+}
+
+template <typename T>
+T structures::CircularArrayQueue<T>::dequeue(void) {
+  if (empty()) {
+    throw(std::out_of_range("Cannot dequeue from empty qeueu"));
+  } else {
+    T data = contents[begin_];
+    begin_ = (begin_ + 1) % max_size_;
+    size_--;
+    return data;
+  }
+}
+
+template <typename T>
+T& structures::CircularArrayQueue<T>::back(void) {
+  if (empty()) {
+    throw(std::out_of_range("Queue is empty"));
+  } else {
+    return contents[end_];
+  }
+}
+
+template <typename T>
+void structures::CircularArrayQueue<T>::clear(void) {
+  begin_ = 0;
+  end_ = -1;
+  size_ = 0;
+}
+
+template <typename T>
+bool structures::CircularArrayQueue<T>::empty(void) {
+  return (size_ == 0);
+}
+
+template <typename T>
+bool structures::CircularArrayQueue<T>::full(void) {
+  return (size_ == max_size_);
+}
+
+template <typename T>
+std::size_t structures::CircularArrayQueue<T>::size(void) {
+  return size_;
+}
+
+template <typename T>
+std::size_t structures::CircularArrayQueue<T>::max_size(void) {
+  return max_size_;
+}
