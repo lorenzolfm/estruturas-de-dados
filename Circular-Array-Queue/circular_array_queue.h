cd09b23acf6c95c59bce0e9b031b72fc7ad9ae18
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-09-20 10:06:51 -0300

    initial commit for circular array queue implementation

---
+++ Circular-Array-Queue/circular_array_queue.h
@@ -0,0 +1,13 @@
+#ifndef STRUCTURES_CIRCULAR_ARRAY_QUEUE_H_
+#define STRUCTURES_CIRCULAR_ARRAY_QUEUE_H_
+
+#include <cstdint>
+#include <stdexcept>
+
+namespace structures {
+template <typename T>
+class CircularArrayQueue {
+ public:
+};
+}  // namespace structures
+#endif
---
+++ Circular-Array-Queue/test_circular_array_queue.cpp
@@ -0,0 +1,15 @@
+#include <stdexcept>
+
+#include "circular_array_queue.h"
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[]) {
+  std::srand(std::time(NULL));
+  ::testing::InitGoogleTest(&argc, argv);
+  ::testing::FLAGS_gtest_death_test_style = "fast";
+  return RUN_ALL_TESTS();
+}
+
+class CircularArrayQueueTest : public ::testing::Test {
+ protected:
+};
