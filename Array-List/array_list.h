00726389b5c69c47a1e0599efd9f6868fe64b365
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-09-20 19:22:35 -0300

    first commit for array list

---
+++ Array-List/array_list.h
@@ -0,0 +1,13 @@
+#ifndef STRUCTURES_ARRAY_LIST_H_
+#define STRUCTURES_ARRAY_LIST_H_
+
+#include <cstdint>
+#include <stdexcept>
+
+namespace structures {
+class ArrayList {
+ public:
+};
+}  // namespace structures
+
+#endif
---
+++ Array-List/test_array_list.cpp
@@ -0,0 +1,17 @@
+#include <stdexcept>
+
+#include "array_list.h"
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[]) {
+  std::srand(std::time(NULL));
+  ::testing::InitGoogleTest(&argc, argv);
+  ::testing::FLAGS_gtest_death_test_style = "fast";
+  return RUN_ALL_TESTS();
+}
+
+class ArrayListTest : public ::testing::Test {
+ protected:
+  //structures::ArrayList<int> circular_queue{20};
+  //structures::ArrayList<int> default_circular_queue{};
+};
