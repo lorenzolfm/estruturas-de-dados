d4ed73848da1a018cf13e8dead2fde37db188993
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-09-30 13:54:53 -0300

    initial commit for string_list

--- Array-List/include/string_list.h
+++ Array-List/include/string_list.h
@@ -1,12 +1,18 @@
 #ifndef STRUCTURES_STRING_LIST_H_
-  #define STRUCTURES_STRING_LIST_H_
+#define STRUCTURES_STRING_LIST_H_
 
-  #include <cstring>
+#include <cstring>
 
-  #include "array_list.h"
+#include "array_list.h"
 
 namespace structures {
 class ArrayListString : public ArrayList<char *> {
+  /*! \brief Classe ArrayListString
+   *            Classe é derivada da classe base ArrayList.
+   *
+   * Essa classe armazena vetores do tipo char.
+     Cada elemento da lista é do tipo ponteiro para vetor de caracteres.
+  */
  public:
 };
 }  // namespace structures
