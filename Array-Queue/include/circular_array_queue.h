1bb5d110f84a51f3a395d24e6568c252965880af
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-09-30 20:57:33 -0300

    WIP: working on including circular_array_queue

---
+++ Array-Queue/include/circular_array_queue.h
@@ -0,0 +1,52 @@
+#ifndef STRUCTURES_CIRCULAR_ARRAY_QUEUE_H_
+#define STRUCTURES_CIRCULAR_ARRAY_QUEUE_H_
+
+#include "array_list.h"
+#include <cstdint>
+#include <stdexcept>
+
+namespace structures {
+template <typename T>
+
+class CircularArrayQueue : public ArrayList<T> {
+ public:
+
+  CircularArrayQueue(void);
+
+  explicit CircularArrayQueue(std::size_t max_size);
+
+  ~CircularArrayQueue(void);
+
+  void enqueue(const T& data);
+
+  T dequeue(void);
+
+  T& back(void);
+
+  void clear(void);
+
+  bool empty(void);
+
+  bool full(void);
+
+  std::size_t size(void);
+
+  std::size_t max_size(void);
+
+ private:
+
+  T* contents;            // Ponteiro para tipo genérico, armazena elementos
+
+  std::size_t size_;      // Tamanho atual da fila
+
+  std::size_t max_size_;  // Tamanho máximo da fila
+
+  int begin_;             // Índice do início
+
+  int end_;               // Índice do fim
+
+  static const auto DEFAULT_SIZE = 10u;  // Tamanho padrão da fila
+};
+}  // namespace structures
+
+#endif
