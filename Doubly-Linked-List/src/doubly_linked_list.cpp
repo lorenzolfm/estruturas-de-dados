b13c254cc3079184b88a3f5937f03fdf7bffe82e
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-10-24 22:51:26 -0300

    WIP

--- Doubly-Linked-List/src/doubly_linked_list.cpp
+++ Doubly-Linked-List/src/doubly_linked_list.cpp
@@ -21,10 +21,16 @@ void structures::DoublyLinkedList<T>::clear(void) {
 }
 
 template <typename T>
+void structures::DoublyLinkedList<T>::push_back(const T& data) {
+  insert(data, size_);
+}
+
+template <typename T>
 void structures::DoublyLinkedList<T>::push_front(const T& data) {
   Node* new_node = new Node(data, head_);
 
   if (new_node == nullptr) {
+    delete new_node;
     throw std::out_of_range("Cannot push front on full list");
   }
 
@@ -34,6 +40,52 @@ void structures::DoublyLinkedList<T>::push_front(const T& data) {
 
   head_ = new_node;
   size_++;
+}
+
+template<typename T>
+void structures::DoublyLinkedList<T>::insert(const T& data, std::size_t index) {
+  if (index == 0) {
+    push_front(data);
+  } else if (index > size_) {
+    throw std::out_of_range("Invalid Index");
+  } else {
+    Node* new_node = new Node(data);
+    Node* before = node(index -1);
+    new_node->next(before->next());
+    before->next(new_node);
+    new_node->previous(before);
+
+    if (new_node->next() != nullptr) {
+      new_node->next()->previous(new_node);
+    }
+
+    size_ ++;
+  }
+}
+
+template <typename T>
+void structures::DoublyLinkedList<T>::insert_sorted(const T& data) {
+  if (empty()) {
+    push_front(data);
+  } else {
+    Node* current = head_;
+    std::size_t position = size_;
+
+    for (auto index = 0; index < size_; index++) {
+      if (!(data > current->data())) {
+        position = index;
+        break;
+      }
+
+      current = current->next();
+    }
+    insert(data, position);
+  }
+}
+
+template<typename T>
+T structures::DoublyLinkedList<T>::pop_back(void) {
+  return pop(size_ - 1);
 }
 
 template<typename T>
@@ -51,10 +103,51 @@ T structures::DoublyLinkedList<T>::pop_front(void) {
   return data;
 }
 
+template<typename T>
+T structures::DoublyLinkedList<T>::pop(std::size_t index) {
+  if (empty()) {
+    throw std::out_of_range("Empty List");
+  }
+
+  if (index > size_ - 1) {
+    throw std::out_of_range("Invalid Index");
+  }
+
+  if (index == 0) {
+    return pop_front();
+  }
+
+  Node* out = node(index);
+  Node* after = out->next();
+  Node* before = out->previous();
+
+  if (before != nullptr) {
+    before->next(after);
+  }
+
+  if (after != nullptr) {
+    after->previous(before);
+  }
+
+  T data = out->data();
+  delete out;
+  size_--;
+  return data;
+}
+
+template<typename T>
+void structures::DoublyLinkedList<T>::remove(const T& data) {
+  pop(find(data));
+}
 
 template <typename T>
 bool structures::DoublyLinkedList<T>::empty(void) const {
   return size_ == 0u;
+}
+
+template <typename T>
+bool structures::DoublyLinkedList<T>::contains(const T& data) const {
+  return find(data) != size();
 }
 
 template <typename T>
--- Doubly-Linked-List/tests/AllTests.cpp
+++ Doubly-Linked-List/tests/AllTests.cpp
@@ -1,4 +1,3 @@
-
 #include "gtest/gtest.h"
 #include "doubly_linked_list.h"
 
@@ -89,8 +88,152 @@ TEST_F(DoublyLinkedListTest, FindReturnsElementIndex) {
 }
 
 TEST_F(DoublyLinkedListTest, FindReturnsSizePlusOneWhenNotThere) {
-  //ASSERT_EQ(list.find(0), 1);
-
   fill();
   ASSERT_EQ(list.find(11), 10);
+}
+
+TEST_F(DoublyLinkedListTest, FindThrowsErrorWhenEmpty) {
+  ASSERT_THROW(list.find(0), std::out_of_range);
+}
+
+TEST_F(DoublyLinkedListTest, ContainsReturnsTrueWhenContains) {
+  fill();
+
+  ASSERT_TRUE(list.contains(0));
+  ASSERT_TRUE(list.contains(5));
+  ASSERT_TRUE(list.contains(9));
+}
+
+TEST_F(DoublyLinkedListTest, ContainsReturnsFalseWhenDontContain) {
+  fill();
+
+  ASSERT_FALSE(list.contains(-1));
+  ASSERT_FALSE(list.contains(10));
+}
+
+TEST_F(DoublyLinkedListTest, ContainsThrowsErrorOnEmptyList) {
+  ASSERT_THROW(list.contains(0), std::out_of_range);
+}
+
+
+TEST_F(DoublyLinkedListTest, InsertThrowsErroWhenOutOfBounds) {
+  ASSERT_THROW(list.insert(1, -1), std::out_of_range);
+  ASSERT_THROW(list.insert(1, 1), std::out_of_range);
+}
+
+TEST_F(DoublyLinkedListTest, InsertAt0PushesFront) {
+  list.insert(0, 0);
+  ASSERT_EQ(list.at(0), 0);
+
+  list.insert(1, 0);
+  ASSERT_EQ(list.at(0), 1);
+}
+
+TEST_F(DoublyLinkedListTest, InsertIncreasesSize) {
+  list.insert(0, 0);
+  ASSERT_EQ(list.size(), 1);
+
+  list.insert(1, 1);
+  ASSERT_EQ(list.size(), 2);
+}
+
+TEST_F(DoublyLinkedListTest, InsertInsertsAtIndex) {
+  for (auto i = 9; i >= 0; i--) {
+    list.push_front(i);
+  }
+
+  list.insert(-5, 1);
+  ASSERT_EQ(list.at(0), 0);
+  ASSERT_EQ(list.at(1), -5);
+  ASSERT_EQ(list.at(2), 1);
+}
+
+TEST_F(DoublyLinkedListTest, InsertSortedInsertsAtTheCorrectPlace) {
+  list.push_back(0);
+  list.push_back(2);
+  list.push_back(4);
+
+  list.insert_sorted(-1);
+  ASSERT_EQ(list.at(0), -1);
+  list.insert_sorted(1);
+  ASSERT_EQ(list.at(2), 1);
+  list.insert_sorted(5);
+  ASSERT_EQ(list.at(5), 5);
+}
+
+TEST_F(DoublyLinkedListTest, PopThrowsErrorWhenEmpty) {
+  ASSERT_THROW(list.pop(0), std::out_of_range);
+}
+
+TEST_F(DoublyLinkedListTest, PopThrowsErrorWhenOutOfBounds) {
+  ASSERT_THROW(list.pop(-1), std::out_of_range);
+
+  list.push_front(0);
+  ASSERT_THROW(list.pop(1), std::out_of_range);
+}
+
+TEST_F(DoublyLinkedListTest, PopDecreasesSize) {
+  for (auto i = 0; i < 10; i++) {
+    list.push_front(i);
+  }
+
+  ASSERT_EQ(list.size(), 10);
+  list.pop(5);
+  ASSERT_EQ(list.size(), 9);
+}
+
+TEST_F(DoublyLinkedListTest, PopIndexZeroPopsFront) {
+  for (auto i = 0; i < 10; i++) {
+    list.push_front(i);
+  }
+
+  for (auto i = 9; i >= 0; i--) {
+    ASSERT_EQ(list.pop(0), i);
+  }
+}
+
+TEST_F(DoublyLinkedListTest, PopPopsAtIndex) {
+  for (auto i = 9; i >= 0; i--) {
+    list.push_front(i);
+  }
+
+  ASSERT_EQ(list.pop(5), 5);
+  ASSERT_EQ(list.pop(0), 0);
+  ASSERT_EQ(list.pop(7), 9);
+}
+
+TEST_F(DoublyLinkedListTest, PopBackRemovesLastNode) {
+  for (auto i = 0; i < 10; i++) {
+    list.push_back(i);
+  }
+
+  for (auto i = 9; i >= 0; i--) {
+    ASSERT_EQ(list.pop_back(), i);
+  }
+}
+
+TEST_F(DoublyLinkedListTest, RemoveThrowsErrorWhenEmpty) {
+  ASSERT_THROW(list.remove(0), std::out_of_range);
+}
+
+TEST_F(DoublyLinkedListTest, RemoveThrowsErrorWhenDoesntContains) {
+  for (auto i = 0; i < 10; i++) {
+    list.push_back(i);
+  }
+
+  ASSERT_THROW(list.remove(-1), std::out_of_range);
+  ASSERT_THROW(list.remove(10), std::out_of_range);
+}
+
+TEST_F(DoublyLinkedListTest, RemovePopsData) {
+  for (auto i = 0; i < 10; i++) {
+    list.push_back(i);
+  }
+
+  list.remove(0);
+  ASSERT_FALSE(list.contains(0));
+  list.remove(5);
+  ASSERT_FALSE(list.contains(5));
+  list.remove(9);
+  ASSERT_FALSE(list.contains(9));
 }
