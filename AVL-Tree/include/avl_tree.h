ed159a1250bc586587992fe5fceac6f0c807bd1d
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-12-05 10:46:57 -0300

    WIP: fuck me

--- AVL-Tree/include/avl_tree.h
+++ AVL-Tree/include/avl_tree.h
@@ -4,131 +4,163 @@
 #include "../../Array-List/include/array_list.h"
 
 namespace structures {
+
 template <typename T>
+//! Árvore AVL
+/*!
+   Implementação da árvore binária semibalanceada AVL utilizando class
+   templates.
+ */
 class AVLTree {
  public:
+  //! Destrutor
+  /*!
+     Destrutor do objeto AVLTree.
+   */
   ~AVLTree(void);
 
+  //! Inserir Dado
+  /*!
+     Insere dado na árvore AVL.
+
+     \param data: Referência constante a tipo genérico (const T&), dado a ser
+     inserido.
+   */
   void insert(const T& data);
 
+  //! Remover Dado
+  /*!
+     Remove dado da árvore AVL.
+
+     \param data: Referência constante a tipo genérico (const T&), dado a ser
+     removido.
+   */
   void remove(const T& data);
 
+  //! Buscar Dado
+  /*!
+     Busca um dado na árvore AVL.
+
+     \param data: Referência constante a tipo genérico (const T&), dado a ser
+     buscado. \return true: Contém o dado. \return false: Não contém o dado.
+   */
   bool contains(const T& data) const;
 
+  //! Vazio
+  /*!
+     Retorna se a árvore está vazia ou não.
+
+     \return true: Árvore está vazia.
+     \return false: Árvore não está vazia.
+   */
   bool empty(void) const;
 
+  //! Tamanho
+  /*!
+     Retorna o tamanho da árvore. Getter do atributo size_.
+
+     \return size: Tamanho da árvore (size_t)
+   */
   std::size_t size(void) const;
 
+  //! Altura
+  /*!
+     Retorna altura da raíz.
+
+     \return altura: Altura da raíz.
+   */
   int height(void) const;
 
+  //! Pré-ordem
+  /*!
+     Percorre a árvore utilizando o algoritmo pré-ordem. Retorna uma lista com
+     os elementos na ordem percorrida.
+
+     \return lista: Lista (ArrayList<T>) com os elementos na ordem em que foram
+     percorridos.
+   */
   ArrayList<T> pre_order(void) const;
 
+  //! Em-ordem
+  /*!
+     Percorre a árvore utilizando o algoritmo em-ordem. Retorna uma lista com os
+     elementos na ordem percorrida.
+
+     \return lista: Lista (ArrayList<T>) com os elementos na ordem em que foram
+     percorridos.
+   */
   ArrayList<T> in_order(void) const;
 
+  //! Pós-ordem
+  /*!
+     Percorre a árvore utilizando o algoritmo pós-ordem. Retorna uma lista com
+     os elementos na ordem percorrida.
+
+     \return lista: Lista (ArrayList<T>) com os elementos na ordem em que foram
+     percorridos.
+   */
   ArrayList<T> post_order(void) const;
 
  private:
   struct Node {
     explicit Node(const T& data)
-        : data_{data}, height_{0}, left_node_{nullptr}, right_node_{nullptr} {}
+        : data_{data}, height_{0}, left_node{nullptr}, right_node{nullptr} {}
 
     T data_;
     int height_;
-    Node* left_node_;
-    Node* right_node_;
+    Node* left_node;
+    Node* right_node;
 
     void insert(const T& data) {
       Node* new_node;
       Node* tree_rotate;
 
-      if (data < data_) {
-        if (left_node_ == nullptr) {
-          new_node = new Node(data);
-          left_node_ = new_node;
-
-          if (left_node_->height() - right_node_->height() > 1) {
-            if (data_ < left_node_->data_)
-              tree_rotate = simpleLeft(this);
-            else
-              tree_rotate = doubleLeft(this);
-
-            if (left_node_ == this)
-              left_node_ = tree_rotate;
-            else
-              right_node_ = tree_rotate;
-          } else {
-            updateHeight(max_height(left_node_, right_node_) + 1);
-          }
-        } else {
-          left_node_->insert(data);
-        }
-        // (data > data_)
+      if (this == nullptr) {
+        new_node = new Node(data);
+        if (new_node == nullptr)
+          throw std::out_of_range("AVL Tree is full");
       } else {
-        if (right_node_ == nullptr) {
-          new_node = new Node(data);
-          right_node_ = new_node;
-
-          if (right_node_->height() - left_node_->height() > 1) {
-            if (data_ < right_node_->data_)
-              tree_rotate = simpleRight(this);
+        if (data_ < data) {
+          left_node->insert(data);
+          if (left_node->height() - right_node->height() > 1) {
+            if (data < left_node->data_)
+              tree_rotate = simpleLeft();
             else
-              tree_rotate = doubleRight(this);
+              tree_rotate = doubleLeft();
 
-            if (right_node_ == this)
-              right_node_ = tree_rotate;
+            if (left_node == this)
+              left_node = tree_rotate;
             else
-              left_node_ = tree_rotate;
+              right_node = tree_rotate;
           } else {
-            updateHeight(max_height(left_node_, right_node_) + 1);
+            new_node->updateHeight(max_height(left_node, right_node) + 1);
           }
         } else {
-          right_node_->insert(data);
+          if (data > data_) {
+            right_node->insert(data);
+
+            if (right_node->height() - left_node->height() > 1) {
+              if (data < right_node->data_)
+                tree_rotate = simpleRight();
+              else
+                tree_rotate = doubleRight();
+
+              if (right_node == this)
+                right_node = tree_rotate;
+              else
+                left_node = tree_rotate;
+            } else {
+              new_node->updateHeight(max_height(left_node, right_node) + 1);
+            }
+          } else {
+            throw std::out_of_range("Key already on tree");
+          }
         }
       }
     }
 
     bool remove(const T& data) {
-      Node* node;
 
-      if (this == nullptr) {
-        return false;
-
-      } else {
-        if (data < data_) {
-          return left_node_->remove(data);
-
-        } else {
-          if (data > data_) {
-            return right_node_->remove(data);
-
-          } else {
-            if (right_node_ != nullptr && left_node_ != nullptr) {
-              node = right_node_->left_node_;
-              data_ = node->data_;
-
-              return right_node_->remove(data_);
-            } else {
-              node = this;
-              if (right_node_ != nullptr) {
-                node = right_node_;
-                data_ = node->data_;
-
-                return right_node_->remove(data_);
-              } else {
-                if (left_node_ != nullptr) {
-                  node = left_node_;
-                  data_ = node->data_;
-
-                  return left_node_->remove(node->data_);
-                } else {
-                  delete this;
-                  return true;
-                }
-              }
-            }
-          }
-        }
-      }
     }
 
     bool contains(const T& data) const {
@@ -136,112 +168,107 @@ class AVLTree {
         return true;
       } else {
         if (data_ < data) {
-          if (right_node_ == nullptr) return false;
+          if (right_node == nullptr) return false;
 
-          return right_node_->contains(data);
+          return right_node->contains(data);
         } else {
-          if (left_node_ == nullptr) return false;
+          if (left_node == nullptr) return false;
+
+          return left_node->contains(data);
         }
-
-        return left_node_->contains(data);
       }
     }
 
-    void updateHeight(std::size_t height) { height_ = height; }
+    void updateHeight(int height) { height_ = height; }
 
-    Node* simpleLeft(Node* node) {
+    Node* simpleLeft() {
       Node* new_root;
 
-      new_root = node->left_node_;
-      node->left_node_ = new_root->left_node_;
-      new_root->right_node_ = node;
+      new_root = left_node;
+      left_node = new_root->left_node;
+      new_root->right_node = this;
 
-      node->updateHeight(max_height(node->left_node_, node->right_node_) + 1);
-      new_root->updateHeight(
-          max_height(new_root->left_node_, node->right_node_) + 1);
+      updateHeight(max_height(left_node, right_node) + 1);
+      new_root->updateHeight(max_height(new_root->left_node, right_node) + 1);
 
       return new_root;
     }
 
-    Node* simpleRight(Node* node) {
+    Node* simpleRight(void) {
       Node* new_root;
 
-      new_root = node->left_node_;
-      node->left_node_ = new_root->right_node_;
-      new_root->left_node_ = node;
+      new_root = left_node;
+      left_node = new_root->right_node;
+      new_root->left_node = this;
 
-      node->updateHeight(max_height(node->right_node_, node->left_node_) + 1);
-      new_root->updateHeight(max_height(new_root->right_node_, node) + 1);
-
-      return new_root;
+      updateHeight(max_height(right_node, left_node) + 1);
+      new_root->updateHeight(max_height(new_root->right_node, this) + 1);
     }
 
-    Node* doubleLeft(Node* node_rotate) {
-      node_rotate->left_node_ = simpleRight(node_rotate->left_node_);
-      return simpleLeft(node_rotate);
+    Node* doubleLeft(void) {
+      left_node = left_node->simpleRight();
+      return simpleLeft();
     }
 
-    Node* doubleRight(Node* node_rotate) {
-      node_rotate->right_node_ = simpleLeft(node_rotate->right_node_);
-      return simpleRight(node_rotate);
-    }
-
-    int max_height(Node* sub_tree_a, Node* sub_tree_b) {
-      int height_sub_tree_a, height_sub_tree_b;
-
-      if (sub_tree_a == nullptr) {
-        height_sub_tree_a = -1;
-      } else {
-        height_sub_tree_a = sub_tree_a->height_;
-      }
-
-      if (sub_tree_b == nullptr) {
-        height_sub_tree_b = -1;
-      } else {
-        height_sub_tree_b = sub_tree_b->height_;
-      }
-
-      if (height_sub_tree_a > height_sub_tree_b) {
-        return height_sub_tree_a;
-      }
-      return height_sub_tree_b;
+    Node* doubleRight(void) {
+      right_node = right_node->simpleLeft();
+      return simpleRight();
     }
 
     void pre_order(ArrayList<T>& array) const {
-      array.push_back(data_);
-      if (left_node_ != nullptr) {
-        left_node_->pre_order(array);
-      }
-      if (right_node_ != nullptr) {
-        left_node_->pre_order(array);
+      if (this != nullptr) {
+        array.push_back(data_);
+        left_node->pre_order(array);
+        right_node->post_order(array);
       }
     }
 
     void in_order(ArrayList<T>& array) const {
-      if (left_node_ != nullptr) {
-        left_node_->pre_order(array);
-      }
-      array.push_back(data_);
-      if (right_node_ != nullptr) {
-        left_node_->pre_order(array);
+      if (this != nullptr) {
+        left_node->in_order(array);
+        array.push_back(data_);
+        right_node->in_order(array);
       }
     }
 
     void post_order(ArrayList<T>& array) const {
-      if (left_node_ != nullptr) {
-        left_node_->pre_order(array);
+      if (this != nullptr) {
+        left_node->post_order(array);
+        right_node->post_order(array);
+        array.push_back(data_);
       }
-      if (right_node_ != nullptr) {
-        left_node_->pre_order(array);
-      }
-      array.push_back(data_);
     }
 
-    int height(void) { return height_; };
+    int height() {
+      if (this == nullptr) return -1;
 
-    Node* root_;
-    std::size_t size_{0u};
+      return height_;
+    }
+
+    int max_height(Node* sub_tree_a, Node* sub_tree_b) {
+      int height_a, height_b;
+
+      if (sub_tree_a == nullptr)
+        height_a = -1;
+      else
+        height_a = sub_tree_a->height_;
+
+      if (sub_tree_b == nullptr)
+        height_b = -1;
+      else
+        height_b = sub_tree_b->height_;
+
+      if (height_a > height_b)
+        return height_a;
+      else
+        return height_b;
+    }
   };
+
+  Node* root;
+  std::size_t size_;
+};
+
 }  // namespace structures
 
 #endif
--- AVL-Tree/src/avl_tree.cpp
+++ AVL-Tree/src/avl_tree.cpp
@@ -1,74 +1,16 @@
 #include "../include/avl_tree.h"
 
 template<typename T>
-structures::AVLTree<T>::~AVLTree(void) {
-  delete this->root_;
-}
-
-template<typename T>
-void structures::AVLTree<T>::insert(const T& data) {
-  if (this->root_ == nullptr) {
-    this->root_ = new Node(data);
-  } else {
-    this->root_->insert(data);
-  }
-  size_++;
-}
-
-template<typename T>
-void structures::AVLTree<T>::remove(const T& data) {
-  if (this->root_->remove(data)) {
-    size_--;
-  }
-}
-
-template<typename T>
-bool structures::AVLTree<T>::contains(const T& data) const {
-  if (this->root_ == nullptr) {
-    return false;
-  } else {
-    return this->root_->contains(data);
-  }
+std::size_t structures::AVLTree<T>::size(void) const {
+    return size_;
 }
 
 template<typename T>
 bool structures::AVLTree<T>::empty(void) const {
-  return size_ == 0u;
+
 }
 
-template<typename T>
-std::size_t structures::AVLTree<T>::size(void) const {
-  return size_;
-}
-
-template<typename T>
-structures::ArrayList<T> structures::AVLTree<T>::pre_order(void) const {
-  structures::ArrayList<T> array{};
-  if (!empty()) {
-    this->root_->pre_order(array);
-  }
-  return array;
-}
-
-template<typename T>
-structures::ArrayList<T> structures::AVLTree<T>::in_order(void) const {
-  structures::ArrayList<T> array{};
-  if (!empty()) {
-    this->root_->in_order(array);
-  }
-  return array;
-}
-
-template<typename T>
-structures::ArrayList<T> structures::AVLTree<T>::post_order(void) const {
-  structures::ArrayList<T> array{};
-  if (!empty()) {
-    this->root_->post_order(array);
-  }
-  return array;
-}
-
-template<typename T>
-int structures::AVLTree<T>::height(void) const {
-  return this->root_->height();
+template <typename T>
+int structures::AVLTree<T>::height() const {
+  return root->height();
 }
