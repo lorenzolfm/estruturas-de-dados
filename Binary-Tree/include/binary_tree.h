406baa2b01419252ed9646e41e8cc5b8838fa8e0
Lorenzo Maturano <maturanolorenzo@gmail.com>
2020-12-05 11:01:12 -0300

    first commit for binary tree refactor

--- Binary-Tree/include/binary_tree.h
+++ Binary-Tree/include/binary_tree.h
@@ -1,8 +1,6 @@
 #ifndef STRUCTURES_BINARY_TREE_H
 #define STRUCTURES_BINARY_TREE_H
 
-#include "../../Array-List/include/array_list.h"
-
 namespace structures {
 template <typename T>
 //! Árvore Binária
@@ -11,7 +9,7 @@ template <typename T>
  */
 class BinaryTree {
  public:
-  ~BinaryTree(void);
+  //~BinaryTree(void);
 
   //! Inserir Dado
   /*!
@@ -19,7 +17,7 @@ class BinaryTree {
 
      \param data: Referência constante de tipo genérico T. Dado a ser inserido.
    */
-  void insert(const T& data);
+  //void insert(const T& data);
 
   //! Remover Dado
   /*!
@@ -27,7 +25,7 @@ class BinaryTree {
 
      \param data: Referência constante de tipo genérico T. Dado a ser removido.
    */
-  void remove(const T& data);
+  //void remove(const T& data);
 
   //! Contém Dado
   /*!
@@ -37,7 +35,7 @@ class BinaryTree {
      buscar. \return true: Dado está contido na árvore. \return false: Dado não
      está contido na árvore.
    */
-  bool contains(const T& data) const;
+  //bool contains(const T& data) const;
 
   //! Árvore Vazia
   /*!
@@ -46,7 +44,7 @@ class BinaryTree {
      \return true: Árvore vazia.
      \return false: Árvore não está vazia.
    */
-  bool empty(void) const;
+  //bool empty(void) const;
 
   //! Tamanho da Árvore
   /*!
@@ -54,7 +52,7 @@ class BinaryTree {
 
      \return tamanho: Tamanho da árvore (size_t).
    */
-  std::size_t size(void) const;
+  //std::size_t size(void) const;
 
   //! Pré-ordem
   /*!
@@ -64,7 +62,7 @@ class BinaryTree {
      \return lista: Lista (ArrayList<T>) com os elementos na ordem em que foram
      percorridos.
    */
-  ArrayList<T> pre_order(void) const;
+  //ArrayList<T> pre_order(void) const;
 
   //! Pós-ordem
   /*!
@@ -74,7 +72,7 @@ class BinaryTree {
      \return lista: Lista (ArrayList<T>) com os elementos na ordem em que foram
      percorridos.
    */
-  ArrayList<T> post_order(void) const;
+  //ArrayList<T> post_order(void) const;
 
   //! Em-ordem
   /*!
@@ -84,116 +82,116 @@ class BinaryTree {
      \return lista: Lista (ArrayList<T>) com os elementos na ordem em que foram
      percorridos.
    */
-  ArrayList<T> in_order(void) const;
+  //ArrayList<T> in_order(void) const;
 
  private:
-  struct Node {
-    explicit Node(const T& data)
-        : data_{data}, left_node_{nullptr}, right_node_{nullptr} {}
+  //struct Node {
+    //explicit Node(const T& data)
+        //: data_{data}, left_node_{nullptr}, right_node_{nullptr} {}
 
-    T data_;
-    Node* left_node_;
-    Node* right_node_;
+    //T data_;
+    //Node* left_node_;
+    //Node* right_node_;
 
-    void insert(const T& data) {
-      Node* new_node;
-      if (data < data_) {
-        if (left_node_ == nullptr) {
-          new_node = new Node(data);
-          left_node_ = new_node;
-        } else {
-          left_node_->insert(data);
-        }
-      } else {
-        if (right_node_ == nullptr) {
-          new_node = new Node(data);
-          right_node_ = new_node;
-        } else {
-          right_node_->insert(data);
-        }
-      }
-    }
+    //void insert(const T& data) {
+      //Node* new_node;
+      //if (data < data_) {
+        //if (left_node_ == nullptr) {
+          //new_node = new Node(data);
+          //left_node_ = new_node;
+        //} else {
+          //left_node_->insert(data);
+        //}
+      //} else {
+        //if (right_node_ == nullptr) {
+          //new_node = new Node(data);
+          //right_node_ = new_node;
+        //} else {
+          //right_node_->insert(data);
+        //}
+      //}
+    //}
 
-    bool remove(const T& data) {
-      if (data == data_) {
-        if ((left_node_ != nullptr) && (right_node_ != nullptr)) {
-          Node* node = right_node_;
-          while (node->left_node_ != nullptr) {
-            node = node->left_node_;
-          }
-          data_ = node->data_;
-          return right_node_->remove(data_);
-        } else {
-          if (right_node_ != nullptr) {
-            data_ = right_node_->data_;
-            return right_node_->remove(data_);
-          } else {
-            if (left_node_ != nullptr) {
-              data_ = left_node_->data_;
-              return left_node_->remove(data_);
-            } else {
-              delete this;
-              return true;
-            }
-          }
-        }
-      } else {
-        if ((right_node_ != nullptr) && (data_ < data)) {
-          return right_node_->remove(data);
-        }
-        if ((left_node_ != nullptr) && (data_ > data)) {
-          return left_node_->remove(data);
-        }
-      }
-      return false;
-    }
+    //bool remove(const T& data) {
+      //if (data == data_) {
+        //if ((left_node_ != nullptr) && (right_node_ != nullptr)) {
+          //Node* node = right_node_;
+          //while (node->left_node_ != nullptr) {
+            //node = node->left_node_;
+          //}
+          //data_ = node->data_;
+          //return right_node_->remove(data_);
+        //} else {
+          //if (right_node_ != nullptr) {
+            //data_ = right_node_->data_;
+            //return right_node_->remove(data_);
+          //} else {
+            //if (left_node_ != nullptr) {
+              //data_ = left_node_->data_;
+              //return left_node_->remove(data_);
+            //} else {
+              //delete this;
+              //return true;
+            //}
+          //}
+        //}
+      //} else {
+        //if ((right_node_ != nullptr) && (data_ < data)) {
+          //return right_node_->remove(data);
+        //}
+        //if ((left_node_ != nullptr) && (data_ > data)) {
+          //return left_node_->remove(data);
+        //}
+      //}
+      //return false;
+    //}
 
-    bool contains(const T& data) const {
-      if (data == data_) {
-        return true;
-      } else {
-        if ((left_node_ != nullptr) && (data_ > data)) {
-          return left_node_->contains(data);
-        } else if ((right_node_ != nullptr) && (data_ < data)) {
-          return right_node_->contains(data);
-        }
-      }
-      return false;
-    }
+    //bool contains(const T& data) const {
+      //if (data == data_) {
+        //return true;
+      //} else {
+        //if ((left_node_ != nullptr) && (data_ > data)) {
+          //return left_node_->contains(data);
+        //} else if ((right_node_ != nullptr) && (data_ < data)) {
+          //return right_node_->contains(data);
+        //}
+      //}
+      //return false;
+    //}
 
-    void pre_order(ArrayList<T>& array) const {
-      array.push_back(data_);
-      if (left_node_ != nullptr) {
-        left_node_->pre_order(array);
-      }
-      if (right_node_ != nullptr) {
-        right_node_->pre_order(array);
-      }
-    }
+    //void pre_order(ArrayList<T>& array) const {
+      //array.push_back(data_);
+      //if (left_node_ != nullptr) {
+        //left_node_->pre_order(array);
+      //}
+      //if (right_node_ != nullptr) {
+        //right_node_->pre_order(array);
+      //}
+    //}
 
-    void in_order(ArrayList<T>& array) const {
-      if (left_node_ != nullptr) {
-        left_node_->in_order(array);
-      }
-      array.push_back(data_);
-      if (right_node_ != nullptr) {
-        right_node_->in_order(array);
-      }
-    }
+    //void in_order(ArrayList<T>& array) const {
+      //if (left_node_ != nullptr) {
+        //left_node_->in_order(array);
+      //}
+      //array.push_back(data_);
+      //if (right_node_ != nullptr) {
+        //right_node_->in_order(array);
+      //}
+    //}
 
-    void post_order(ArrayList<T>& array) const {
-      if (left_node_ != nullptr) {
-        left_node_->post_order(array);
-      }
-      if (right_node_ != nullptr) {
-        right_node_->post_order(array);
-      }
-      array.push_back(data_);
-    }
-  };
+    //void post_order(ArrayList<T>& array) const {
+      //if (left_node_ != nullptr) {
+        //left_node_->post_order(array);
+      //}
+      //if (right_node_ != nullptr) {
+        //right_node_->post_order(array);
+      //}
+      //array.push_back(data_);
+    //}
+  //};
 
-  Node* root_{nullptr};
-  std::size_t size_{0u};
+  //Node* root_{nullptr};
+  //std::size_t size_{0u};
 };
 }  // namespace structures
 #endif
