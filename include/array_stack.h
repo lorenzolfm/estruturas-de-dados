dfc12b607f9d5d7db5496f6ab6ae34014191ab5b
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-01-22 15:58:45 -0300

    WIP

---
+++ include/array_stack.h
@@ -0,0 +1,126 @@
+#ifndef INCLUDE_ARRAY_STACK_H_
+#define INCLUDE_ARRAY_STACK_H_
+
+#include <cstdint>    // std::size_t
+#include <stdexcept>  // C++ exceptions
+
+namespace structures {
+template <typename T>
+//! Classe ArrayStack
+/*!
+    Classe pilha em vetor, com tipo genérico, implementada utilizando um vetor
+    estático.
+ */
+class ArrayStack {
+ public:
+  //! Construtor padrão
+  /*!
+      Cria uma pilha utilizando o atributo de tamanho padrão como tamanho máximo
+     da pilha
+   */
+  ArrayStack(void);
+
+  //! Construtor com parâmetro de tamanho máximo
+  /*!
+      Cria uma pilha utilizando o parâmetro max_size como tamanho máximo da
+      pilha.
+     \param max_size: Tamanho máximo da pilha (size_t)
+   */
+  explicit ArrayStack(std::size_t max_size);
+
+  //! Destrutor
+  /*!
+    Usado para destruir o objeto quando não for mais utilizado
+   */
+  ~ArrayStack(void);
+
+  //! Método empilha
+  /*!
+     Empilha elemento no topo da pilha, se houver espaço. Se não houver espaço,
+     lança exceção (out_of_range).
+     \param data: Referência constante para o elemento a ser empilhado (const
+     T&)
+   */
+  void push(const T& data);
+
+  //! Método desempilha.
+  /*!
+     Remove dado no topo da pilha o retorna, se houver elementos na pilha. Se
+     não houver elementos, lança exceção (out_of_range).
+     \return Elemento removido (T).
+   */
+  T pop(void);
+
+  //! Método topo.
+  /*!
+    Retorna por referência o elemento no topo.
+    \return Referência ao elemento no topo (T&).
+   */
+  T& top(void);
+
+  //! Método vazio
+  /*!
+    Verifica se a pilha está vazia. Retorna verdadeiro se a pilha estiver vazia,
+    caso contrário retorna falso.
+    \return true: Pilha vazia (bool).
+    \return false: Pilha contém elementos (bool).
+   */
+  bool empty(void) const;
+
+  //! Método cheio
+  /*!
+    Verifica se pilha está cheia. Retorna verdadeiro se a pilha estiver cheia,
+    caso contrário retorna falso.
+    \return true: Pilha cheia (bool)
+    \return false: Pilha não está cheia (bool).
+   */
+  bool full(void) const;
+
+  //! Método limpar.
+  /*!
+     Limpa a pilha. Atribui -1 para o tamanho da pilha.
+   */
+  void clear(void);
+
+  //! Método tamanho
+  /*!
+    Getter do atributo size_, retorna o tamanho atual da pilha.
+    \return Tamanho da pilha (size_t).
+   */
+  std::size_t size(void) const;
+
+  //! Método tamanho máximo.
+  /*!
+    Getter do atributo max_size_, retorna tamanho máximo da pilha.
+    \return tamanho máximo da pilha (size_t)
+   */
+  std::size_t max_size(void) const;
+
+ private:
+  //! Conteúdo
+  /*!
+     Ponteiro para tipo genérico, armazena elementos.
+  */
+  T* contents;
+
+  //! Topo
+  /*!
+     Tamanho atual da pilha. Inicializado em -1.
+  */
+  int top_;
+
+  //! Tamanho máximo
+  /*!
+     Capacidade máxima da pilha.
+  */
+  std::size_t max_size_;
+
+  //! Tamanho máximo padrão
+  /*!
+     Capacida máxima da pilha, usada pelo construtor padrão.
+  */
+  static const auto DEFAULT_MAX_SIZE = 10u;
+};
+}  // namespace structures
+
+#endif  // INCLUDE_ARRAY_STACK_H_
---
+++ src/array_stack.cpp
@@ -0,0 +1,74 @@
+#include "../include/array_stack.h"
+
+#include <cstdint>    // std::size_t
+#include <stdexcept>  // C++ exceptions
+
+template <typename T>
+structures::ArrayStack<T>::ArrayStack(void) {
+  max_size_ = DEFAULT_MAX_SIZE;
+  top_ = -1;
+  contents = new T[max_size_];
+}
+
+template <typename T>
+structures::ArrayStack<T>::ArrayStack(size_t max_size) {
+  max_size_ = max_size;
+  top_ = -1;
+  contents = new T[max_size_];
+}
+
+template <typename T>
+structures::ArrayStack<T>::~ArrayStack(void) {
+  delete[] contents;
+}
+
+template <typename T>
+void structures::ArrayStack<T>::push(const T& data) {
+  if (full()) {
+    throw std::out_of_range("Stack Overflow");
+  }
+  contents[++top_] = data;
+}
+
+template <typename T>
+T structures::ArrayStack<T>::pop(void) {
+  if (empty()) {
+    throw std::out_of_range("Can't pop from empty stack");
+  }
+  return contents[top_--];
+}
+
+template <typename T>
+T& structures::ArrayStack<T>::top(void) {
+  if (empty()) {
+    throw std::out_of_range("Stack is empty!");
+  }
+  return contents[top_];
+}
+
+template <typename T>
+bool structures::ArrayStack<T>::empty(void) const {
+  return top_ == -1;
+}
+
+template <typename T>
+bool structures::ArrayStack<T>::full(void) const {
+  return size() == max_size_;
+}
+
+template <typename T>
+void structures::ArrayStack<T>::clear(void) {
+  top_ = -1;
+}
+
+template <typename T>
+std::size_t structures::ArrayStack<T>::size(void) const {
+  return top_ + 1;
+}
+
+template <typename T>
+std::size_t structures::ArrayStack<T>::max_size(void) const {
+  return max_size_;
+}
+
+template class structures::ArrayStack<int>;
